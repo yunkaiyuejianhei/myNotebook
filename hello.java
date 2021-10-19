41bb26382927068f0c5652179b85b547b3478384
yunkai <1547029907@qq.com>
2021-10-19 14:31:22 +0800

    master test

--- hello.java
+++ hello.java
@@ -1,6 +1,7 @@
 public class hello{
 	public static void main(String[]args){
-	   System.out.println("\nhello word");
+	System.out.println("\nhello word!\n");
+	System.out.println("master test");
 	}
 
 }
