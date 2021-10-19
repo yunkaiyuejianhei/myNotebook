cb9859f4c5cf83b8382f15ebf8aad31bb7c2d651
yunkai <1547029907@qq.com>
2021-10-19 15:32:22 +0800

    null

--- hello.java
+++ hello.java
@@ -1,8 +1,6 @@
 public class hello{
 	public static void main(String[]args){
-	System.out.println("\nhello word!\n");
-	System.out.println("master test");
-	System.out.println("hot-fix test");
+	System.out.println("hello word!");
 	}
 
 }
